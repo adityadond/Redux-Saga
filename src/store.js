251211264c792f8464b963bb767355fb41b4b92c
Aditya Dond <adityadond281@gmail.com>
2024-03-16 10:40:30 +0530

    Added redux webtool to track state in app

--- src/store.js
+++ src/store.js
@@ -2,10 +2,14 @@ import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { watchFetchData, watchFetchProductDetails } from "./helloSagas";
 import { rootreducer } from "./reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootreducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  rootreducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 sagaMiddleware.run(watchFetchData);
 sagaMiddleware.run(watchFetchProductDetails);
 export default store;
