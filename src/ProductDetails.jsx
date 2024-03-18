bd992d37c0682739824843e82293ba7e2bfc8e3b
Aditya Dond <adityadond281@gmail.com>
2024-03-18 18:54:02 +0530

    add changes to product details page

--- src/ProductDetails.jsx
+++ src/ProductDetails.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import useProductDetail from "./useProductDetail";
 import { useParams } from "react-router-dom";
+import "./ProductDetails.css"; // Import CSS file for styling
 
 function ProductDetails() {
   const { id } = useParams();
   const { state, error, loading } = useProductDetail(id);
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -13,19 +15,29 @@ function ProductDetails() {
     return <div>Error: {error}</div>;
   }
 
+  const { category, description, image, price, rating, title } =
+    state.productdetails;
+
   return (
-    <div>
-      {" "}
-      <div>
-        <h2>Product Details</h2>
-        <ul>
-          {Object.entries(state.productdetails).map(([key, value]) => (
-            <li key={key}>
-              <strong>{key}:</strong>{" "}
-              {typeof value === "object" ? JSON.stringify(value) : value}
-            </li>
-          ))}
-        </ul>
+    <div className="product-details-container">
+      <h2 className="product-details-title">{title}</h2>
+      <div className="product-details-content">
+        <div className="product-details-image">
+          <img src={image} alt={title} />
+        </div>
+        <div className="product-details-info">
+          <p>
+            <strong>Category:</strong>{" "}
+            <span className="category">{category}</span>
+          </p>
+          <p>
+            <strong>Description:</strong> {description}
+          </p>
+          <p>
+            <strong>Price:</strong> ${price}
+          </p>
+          <p></p>
+        </div>
       </div>
     </div>
   );
